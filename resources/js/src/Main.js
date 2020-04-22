f1659e87d07047c39b33c1f5b1aecf64dbc7001a
haidi20 <nhaidii@yahoo.com>
2020-04-23 07:29:36 +0800

    first setup project

---
+++ resources/js/src/Main.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+// import Route from './route/Web';
+
+class Main extends Component {
+    render(){
+        return(
+            // <Route />
+            <div>
+                <h2>Setup Project</h2>
+            </div>
+        )
+    }
+}
+
+export default Main;
